8ea34ef4e1013fe025a07466590aa8935ade0e19
Jorge Díez de la Fuente <buker@stuker.es>
2023-03-19 19:11:52 +0000

    Parser error test

---
+++ client/parsers/error_test.go
@@ -0,0 +1,29 @@
+package parsers_test
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/SuperBuker/terraform-provider-dns-he-net/client/parsers"
+	"github.com/stretchr/testify/require"
+)
+
+func TestError(t *testing.T) {
+	t.Run("missing error", func(t *testing.T) {
+		data, err := ioutil.ReadFile("../testing_data/main.html")
+		require.NoError(t, err)
+
+		errorString, err := parsers.ParseError(data)
+		require.Equal(t, "", errorString)
+		require.NoError(t, err)
+	})
+
+	t.Run("error present", func(t *testing.T) {
+		data, err := ioutil.ReadFile("../testing_data/login_totp_err.html")
+		require.NoError(t, err)
+
+		errorString, err := parsers.ParseError(data)
+		require.Equal(t, "The token supplied is invalid.", errorString)
+		require.NoError(t, err)
+	})
+}
