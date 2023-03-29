190ae127d498fdca43df0700e543fa20dda3bc3b
Jorge Díez de la Fuente <buker@stuker.es>
2023-03-29 15:44:45 +0000

    Record parsing tests

--- client/parsers/records.go
+++ client/parsers/records.go
@@ -17,9 +17,7 @@ func parseRecordNode(node *html.Node) (record models.Record) { // missing error
 	for c = node.FirstChild; ; c = c.NextSibling {
 		if c == nil {
 			return
-		} else if c.Type != html.ElementNode {
-			// pass
-		} else if c.Data != "td" {
+		} else if c.Type != html.ElementNode || c.Data != "td" {
 			// pass
 		} else if htmlquery.SelectAttr(c, "class") == "hidden" {
 			parentId, _ := strconv.Atoi(htmlquery.InnerText(c)) // To improve
@@ -31,9 +29,7 @@ func parseRecordNode(node *html.Node) (record models.Record) { // missing error
 	for c = c.NextSibling; ; c = c.NextSibling {
 		if c == nil {
 			return
-		} else if c.Type != html.ElementNode {
-			// pass
-		} else if c.Data != "td" {
+		} else if c.Type != html.ElementNode || c.Data != "td" {
 			// pass
 		} else if htmlquery.SelectAttr(c, "class") == "hidden" {
 			recordId, _ := strconv.Atoi(htmlquery.InnerText(c)) // To improve
@@ -46,9 +42,7 @@ func parseRecordNode(node *html.Node) (record models.Record) { // missing error
 	for c = c.NextSibling; ; c = c.NextSibling {
 		if c == nil {
 			return
-		} else if c.Type != html.ElementNode {
-			// pass
-		} else if c.Data != "td" {
+		} else if c.Type != html.ElementNode || c.Data != "td" {
 			// pass
 		} else if class := htmlquery.SelectAttr(c, "class"); len(class) == 0 {
 			// pass
@@ -65,15 +59,11 @@ func parseRecordNode(node *html.Node) (record models.Record) { // missing error
 	for c = c.NextSibling; ; c = c.NextSibling {
 		if c == nil {
 			return
-		} else if c.Type != html.ElementNode {
-			// pass
-		} else if c.Data != "td" {
+		} else if c.Type != html.ElementNode || c.Data != "td" {
 			// pass
 		} else if htmlquery.SelectAttr(c, "align") == "center" {
 			for d := c.FirstChild; d != nil; d = d.NextSibling {
-				if d.Type != html.ElementNode {
-					// pass
-				} else if d.Data != "span" {
+				if d.Type != html.ElementNode || d.Data != "span" {
 					// pass
 				} else if data := htmlquery.SelectAttr(d, "data"); len(data) != 0 {
 					record.RecordType = strings.ToUpper(data)
@@ -87,12 +77,9 @@ func parseRecordNode(node *html.Node) (record models.Record) { // missing error
 	for c = c.NextSibling; ; c = c.NextSibling {
 		if c == nil {
 			return
-		} else if c.Type != html.ElementNode {
-			// pass
-		} else if c.Data != "td" {
+		} else if c.Type != html.ElementNode || c.Data != "td" {
 			// pass
 		} else if htmlquery.SelectAttr(c, "align") == "left" {
-			//fmt.Println("ttl")
 			recordTTL, _ := strconv.Atoi(htmlquery.InnerText(c)) // To improve
 			record.TTL = uint(recordTTL)
 			break
@@ -102,16 +89,13 @@ func parseRecordNode(node *html.Node) (record models.Record) { // missing error
 	for c = c.NextSibling; ; c = c.NextSibling {
 		if c == nil {
 			return
-		} else if c.Type != html.ElementNode {
-			// pass
-		} else if c.Data != "td" {
+		} else if c.Type != html.ElementNode || c.Data != "td" {
 			// pass
 		} else if htmlquery.SelectAttr(c, "align") == "center" {
-			//fmt.Println("priority", htmlquery.InnerText(c))
 			p := htmlquery.InnerText(c)
 			if priority, err := strconv.Atoi(p); err == nil {
 				p := uint16(priority)
-				record.Priority = &p // To improve
+				record.Priority = &p
 			} else if p != "-" {
 				// this is an error
 				fmt.Println(err)
@@ -123,14 +107,11 @@ func parseRecordNode(node *html.Node) (record models.Record) { // missing error
 	for c = c.NextSibling; ; c = c.NextSibling {
 		if c == nil {
 			return
-		} else if c.Type != html.ElementNode {
-			// pass
-		} else if c.Data != "td" {
+		} else if c.Type != html.ElementNode || c.Data != "td" {
 			// pass
 		} else if htmlquery.SelectAttr(c, "align") != "left" {
 			// pass
 		} else if data := htmlquery.SelectAttr(c, "data"); len(data) != 0 {
-			//fmt.Println("data", data)
 			record.Data = data
 			break
 		}
@@ -139,13 +120,10 @@ func parseRecordNode(node *html.Node) (record models.Record) { // missing error
 	for c = c.NextSibling; ; c = c.NextSibling {
 		if c == nil {
 			return
-		} else if c.Type != html.ElementNode {
-			// pass
-		} else if c.Data != "td" {
+		} else if c.Type != html.ElementNode || c.Data != "td" {
 			// pass
 		} else if htmlquery.SelectAttr(c, "class") == "hidden" {
-			//fmt.Println("dynamic", htmlquery.InnerText(c))
-			record.Dynamic, _ = strconv.ParseBool(htmlquery.InnerText(c))
+			record.Dynamic, _ = strconv.ParseBool(htmlquery.InnerText(c)) // To improve
 			break
 		}
 	}
@@ -154,7 +132,13 @@ func parseRecordNode(node *html.Node) (record models.Record) { // missing error
 }
 
 func GetRecords(doc *html.Node) ([]models.Record, error) {
-	q := `//div[@id="dns_main_content"]/table[@class="generictable"]/tbody/tr[@class]`
+	q := `//div[@id="dns_main_content"]/table[@class="generictable"]`
+
+	if table := htmlquery.FindOne(doc, q); table == nil {
+		return nil, &ErrNotFound{q}
+	}
+
+	q = `//div[@id="dns_main_content"]/table[@class="generictable"]/tbody/tr[@class]`
 	nodes := htmlquery.Find(doc, q)
 
 	records := make([]models.Record, len(nodes))
