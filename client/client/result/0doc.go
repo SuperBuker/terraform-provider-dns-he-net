79c83dd366029e41aa1dc59ad563660ff5530d6e
Jorge Díez de la Fuente <buker@stuker.es>
2023-05-03 22:57:32 +0000

    Cosmetic fixes

--- client/client/result/0doc.go
+++ client/client/result/0doc.go
@@ -1,4 +1,4 @@
 // result is an enhancement over the go-resty Request.Result to provide
-// persistance between the different OnAfterResponse stages.
+// persistence between the different OnAfterResponse stages.
 
 package result
