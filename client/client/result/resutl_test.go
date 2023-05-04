bd57c6ad70d9c6ffcd2aae7888ae8c6f37201434
Jorge Díez de la Fuente <buker@stuker.es>
2023-05-04 10:43:46 +0000

    ResultX safe multi-Inits

--- client/client/result/resutl_test.go
+++ client/client/result/resutl_test.go
@@ -2,6 +2,7 @@ package result_test
 
 import (
 	"bytes"
+	"errors"
 	"testing"
 
 	"github.com/SuperBuker/terraform-provider-dns-he-net/client/client/result"
@@ -128,4 +129,77 @@ func TestNoResultX(t *testing.T) {
 	// Compare result output
 	assert.Nil(t, resp.Result())
 	assert.Equal(t, resp.Result(), result.Result(resp))
+}
+
+func TestResultXRetry(t *testing.T) {
+	// This test is required because it should be safe to initialise twice ResultX
+	// on the same request. This is because it will be reinitialised on a retry.
+
+	client := setup(t)
+	defer teardown(t)
+
+	data := `<html><head><title>Example Domain</title></head><body>some data</body></html>`
+	responder := httpmock.NewStringResponder(200, data)
+	url := "https://example.com"
+	httpmock.RegisterResponder("GET", url, responder)
+
+	// Parse html
+	client.OnAfterResponse(func(c *resty.Client, resp *resty.Response) (err error) {
+		if resp.StatusCode() == 200 {
+			err = errors.Join(result.Init(resp), result.Init(resp))
+		}
+		return
+	})
+
+	t.Run("Without Result", func(t *testing.T) {
+		resp, err := client.R().Get(url)
+		require.NoError(t, err)
+
+		body := resp.Body()
+
+		// Compare parsing results
+		var doc *html.Node
+		doc, err = htmlquery.Parse(bytes.NewReader(body))
+		require.NoError(t, err)
+
+		assert.Equal(t, doc, result.Body(resp))
+
+		// Validate parsing
+		q := `//body`
+		node := htmlquery.FindOne(doc, q)
+		assert.Equal(t, "some data", htmlquery.InnerText(node))
+
+		res, ok := resp.Result().(result.ResultX)
+		require.True(t, ok)
+
+		// Compare result output
+		assert.Nil(t, res.Result)
+		assert.Equal(t, res.Result, result.Result(resp))
+	})
+
+	t.Run("With Result", func(t *testing.T) {
+		resp, err := client.R().SetResult(struct{}{}).Get(url)
+		require.NoError(t, err)
+
+		body := resp.Body()
+
+		// Compare parsing results
+		var doc *html.Node
+		doc, err = htmlquery.Parse(bytes.NewReader(body))
+		require.NoError(t, err)
+
+		assert.Equal(t, doc, result.Body(resp))
+
+		// Validate parsing
+		q := `//body`
+		node := htmlquery.FindOne(doc, q)
+		assert.Equal(t, "some data", htmlquery.InnerText(node))
+
+		res, ok := resp.Result().(result.ResultX)
+		require.True(t, ok)
+
+		// Compare result output
+		assert.NotNil(t, res.Result)
+		assert.Equal(t, res.Result, result.Result(resp))
+	})
 }
